c14071b0b24efea9eb3a8bb3f52c16d685950274
Iakiv Kramarenko <yashaka@gmail.com>
2013-03-29 21:57:17 +0200

    solved but optimization is bad, without the latter it works better

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,41 @@
 (ns clojure-course-task02.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
+(defn separate
+  "Returns a vector:
+   [ (filter f s), (filter (complement f) s) ]"
+  [f s]
+  [(filter f s) (filter (complement f) s)])
+
+(defn dirs-and-files [path]
+  (->> 
+    (seq (.listFiles (io/file path)))
+    (separate #(.isDirectory %))))
 
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [[dirs files] (dirs-and-files path)]
+
+    (->> dirs
+         (map #(.getPath %))
+         (map #(future (find-files file-name %)))
+         (map deref)
+         (concat (->> files
+                      (map #(.getName %))
+                      (filter #(re-find (re-pattern file-name) %))))
+         (flatten))))
+
+(defn pfind-files [file-name path]
+  (let [[dirs files] (dirs-and-files path)]
+    (->> files
+         (map #(.getName %))
+         (filter #(re-find (re-pattern file-name) %))
+         (concat (->> dirs
+                      (map #(.getPath %))
+                      (map #(future (find-files file-name %)))
+                      (map deref)
+                      (flatten))))))
+
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
